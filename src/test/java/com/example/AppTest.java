687e8f21cf8152031d51d11a70c8e325aebd628e
Pranav <pranav@gmail.com>
2026-06-17 10:58:06 +0530

    second21 commit of HelloMaven project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testGreeting() {
+        String greeting = "Hello World";
+        assertEquals("Hello World", greeting);
+    }
+}
